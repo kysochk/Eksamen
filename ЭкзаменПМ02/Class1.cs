bf8d51f595f1e2dd76764070a8b449f1f090e2fb
Ky So <klimochkinaff@mail.ru>
2021-07-05 11:28:41 +0300

    добавление отладочных классов

--- ЭкзаменПМ02/Class1.cs
+++ ЭкзаменПМ02/Class1.cs
@@ -12,7 +12,7 @@ namespace ЭкзаменПМ02
     {
         public string a;
         /// <summary>
-        /// пути и стоимость перемещения
+        /// пути и время(дни) перемещения
         /// </summary>
         public struct Struct
         {
@@ -28,53 +28,61 @@ namespace ЭкзаменПМ02
 
         }
         /// <summary>
-        /// Рабочий метод по построению путей и подсчета длины.
+        /// Основной рабочий метод
         /// </summary>
-        public void Work()
+        public void resh()
         {
-            List<Struct> LPath;//лист путей
-            List<Struct> StQ = Input();//лист исходных данных 
-            LPath = StQ.FindAll(x => x.p1 == StQ[MinElem(StQ)].p1);//запись точки начала в лист путей
-            List<List<Struct>> LPathFunc = new List<List<Struct>>();//лист путей и функций
-            foreach (Struct rb in LPath)//построение путей из начальных возможных перемещений
+            try
             {
-                CreatePath(StQ, rb);//Построение пути
-                LPathFunc.Add(Branches(StQ, a));//Построение ветвей
-                a = "";
-            }
-
-            Debug.WriteLine("Все пути: ");
-            for (int i = 0; i < LPathFunc.Count; i++)
-            {
-                foreach (Struct path in LPathFunc[i])
+                List<Struct> LPath;
+                List<Struct> StQ = Input();//лист исходных данных 
+                LPath = StQ.FindAll(x => x.p1 == StQ[MinElem(StQ)].p1);
+                List<List<Struct>> LPathFunc = new List<List<Struct>>();
+                foreach (Struct rb in LPath)
                 {
-                    Debug.Write(path.p1 + " - " + path.p2 + ";(" + path.length + ") ");
+                    CreatePath(StQ, rb);
+                    LPathFunc.Add(Branches(StQ, a));
+                    a = "";
                 }
-                Debug.WriteLine("");
-            }
 
-
-            int max = LPathFunc[0][0].length, maxind = 0;
-            for (int i = 0; i < LPath.Count; i++)// подсчет стоимости путей
-            {
-                if (LenFunc(LPathFunc[i]) >= max)// выбор самого большого
+                Debug.WriteLine("Возможные пути: ");
+                for (int i = 0; i < LPathFunc.Count; i++)
                 {
-                    max = LenFunc(LPathFunc[i]);
-                    maxind = i;
+                    foreach (Struct path in LPathFunc[i])
+                    {
+                        Debug.Write(path.p1 + " - " + path.p2);
+                    }
+                    Debug.WriteLine("");
                 }
+
+
+                int max = LPathFunc[0][0].length, maxind = 0;
+                for (int i = 0; i < LPath.Count; i++)// подсчет стоимости путей
+                {
+                    if (LenFunc(LPathFunc[i]) >= max)// выбор самого большого
+                    {
+                        max = LenFunc(LPathFunc[i]);
+                        maxind = i;
+                    }
+                }
+                Debug.WriteLine("Критический путь (в днях):  " + max);
+                Debug.WriteLine("Номер пути: " + maxind);
+                vivod(LPathFunc, maxind, max);//Запись в файл решения
             }
-            Debug.WriteLine("Максимум " + max);
-            Debug.WriteLine("Номер максимума " + maxind);
-            vivod(LPathFunc, maxind, max);//Запись в файл решения
-            Debug.Listeners.Clear();
+            catch
+            {
+                Debug.WriteLine("Ошибка в модуле решения");
+                Environment.Exit(1);
+
+
+            }
         }
 
         /// <summary>
-        /// Поиск начальной точки.Путем взятия самого маленького элемента из первого столбца, которого нет во втором
+        /// Поиск начальной точки
         /// </summary>
         /// <param name="StQ"></param>
         /// <returns></returns>
-
         public int MinElem(List<Struct> StQ)
         {
             int min = StQ[0].p1, minind = 0;
@@ -113,51 +121,70 @@ namespace ЭкзаменПМ02
         /// <returns></returns>
         public List<Struct> Input()
         {
-            Debug.WriteLine("\n\nЧтение:");
-            List<Struct> StQ = new List<Struct>();
-            using (StreamReader sr = new StreamReader("Ввод.csv"))
+            try
             {
-                while (sr.EndOfStream != true)
+                Debug.WriteLine("Исходные данные:");
+                List<Struct> StQ = new List<Struct>();
+                using (StreamReader sr = new StreamReader(@"Ввод.csv"))
                 {
-                    string[] s1 = sr.ReadLine().Split(';');
-                    string[] s2 = s1[0].Split('-');
-                    Debug.WriteLine(s2[0] + " - " + s2[1] + "; " + s1[1]);
-                    StQ.Add(new Struct { p1 = Convert.ToInt32(s2[0]), p2 = Convert.ToInt32(s2[1]), length = Convert.ToInt32(s1[1]) });
+                    while (sr.EndOfStream != true)
+                    {
+                        string[] s1 = sr.ReadLine().Split(';');
+                        string[] s2 = s1[0].Split('-');
+                        Debug.WriteLine(s2[0] + " - " + s2[1] + "; Day: " + s1[1]);
+                        StQ.Add(new Struct { p1 = Convert.ToInt32(s2[0]), p2 = Convert.ToInt32(s2[1]), length = Convert.ToInt32(s1[1]) });
+                    }
                 }
+                return StQ;
             }
-            return StQ;
+            catch
+            {
+                List<Struct> StQ = new List<Struct>();
+                Debug.WriteLine("Ошибка в модуле чтения");
+                Environment.Exit(1);
+                return StQ; //обход ошибки
+            }
         }
         /// <summary>
-        /// Метод построения пути Работает рекурсивно
+        /// Метод построения пути 
         /// </summary>
         /// <param name="StQ"></param>
         /// <param name="minel"></param>
         /// <returns></returns>
         public int CreatePath(List<Struct> StQ, Struct minel)
         {
-            int Lenght = 0;
-            Struct MoveVar = StQ.Find(x => x.p1 == minel.p1 && x.p2 == minel.p2);//возможные варианты передвижения
-            a += MoveVar.p1.ToString() + "-" + MoveVar.p2.ToString();//передижение
-            if (MoveVar.p2 == StQ[MaxElem(StQ)].p2)//местонахождение
+            try
             {
-                a += ";";
-                return MoveVar.length;
-            }
-            else
-            {
-                for (int i = 0; i < StQ.Count; i++)//стоимость перемещения
+                int Lenght = 0;
+                Struct MoveVar = StQ.Find(x => x.p1 == minel.p1 && x.p2 == minel.p2);
+                a += MoveVar.p1.ToString() + "-" + MoveVar.p2.ToString();//переход
+                if (MoveVar.p2 == StQ[MaxElem(StQ)].p2)
                 {
-                    if (StQ[i].p1 == MoveVar.p2)
+                    a += ";";
+                    return MoveVar.length;
+                }
+                else
+                {
+                    for (int i = 0; i < StQ.Count; i++)//стоимость перемещения
                     {
-                        a += ",";
-                        Lenght = CreatePath(StQ, StQ[i]) + MoveVar.length;
+                        if (StQ[i].p1 == MoveVar.p2)
+                        {
+                            a += ",";
+                            Lenght = CreatePath(StQ, StQ[i]) + MoveVar.length;
+                        }
                     }
                 }
+                return Lenght;
             }
-            return Lenght;
+            catch
+            {
+                Debug.WriteLine("Ошибка в модуле построения путей");
+                Environment.Exit(1);
+                return 1;
+            }
         }
         /// <summary>
-        /// Сумма длины пути
+        /// Поиск суммы длины пути
         /// </summary>
         /// <param name="StQ"></param>
         /// <returns></returns>
@@ -178,63 +205,84 @@ namespace ЭкзаменПМ02
         /// <returns></returns>
         public List<Struct> Branches(List<Struct> StQ, string s)
         {
-            List<List<Struct>> LBr = new List<List<Struct>>();
-            string[] s1 = s.Split(';');
-            foreach (string st1 in s1)
+            try
             {
-                if (st1 != "")
+                List<List<Struct>> LBr = new List<List<Struct>>();
+                string[] s1 = s.Split(';');
+                foreach (string st1 in s1)
                 {
-                    LBr.Add(new List<Struct>());
-                    string[] s2 = st1.Split(',');
-                    foreach (string str2 in s2)
+                    if (st1 != "")
                     {
-                        if (str2 != "")
+                        LBr.Add(new List<Struct>());
+                        string[] s2 = st1.Split(',');
+                        foreach (string str2 in s2)
                         {
-                            string[] str3 = str2.Split('-');
-                            LBr[LBr.Count - 1].Add(StQ.Find(x => x.p1 == Convert.ToInt32(str3[0]) && x.p2 == Convert.ToInt32(str3[1])));
+                            if (str2 != "")
+                            {
+                                string[] str3 = str2.Split('-');
+                                LBr[LBr.Count - 1].Add(StQ.Find(x => x.p1 == Convert.ToInt32(str3[0]) && x.p2 == Convert.ToInt32(str3[1])));
+                            }
                         }
                     }
                 }
-            }
-            foreach (List<Struct> l in LBr)
-            {
-                if (l[0].p1 != StQ[MinElem(StQ)].p1)
+                foreach (List<Struct> l in LBr)
                 {
-                    foreach (List<Struct> l1 in LBr)
+                    if (l[0].p1 != StQ[MinElem(StQ)].p1)
                     {
-                        if (l1[0].p1 == StQ[MinElem(StQ)].p1)
+                        foreach (List<Struct> l1 in LBr)
                         {
-                            l.InsertRange(0, l1.FindAll(x => l1.IndexOf(x) <= l1.FindIndex(y => y.p2 == l[0].p1)));
+                            if (l1[0].p1 == StQ[MinElem(StQ)].p1)
+                            {
+                                l.InsertRange(0, l1.FindAll(x => l1.IndexOf(x) <= l1.FindIndex(y => y.p2 == l[0].p1)));
+                            }
                         }
                     }
                 }
-            }
-            int max = LBr[0][0].length, maxind = 0;
-            for (int i = 0; i < LBr.Count; i++)
-            {
-                if (LenFunc(LBr[i]) >= max)
+                int max = LBr[0][0].length, maxind = 0;
+                for (int i = 0; i < LBr.Count; i++)
                 {
-                    max = LenFunc(LBr[i]);
-                    maxind = i;
+                    if (LenFunc(LBr[i]) >= max)
+                    {
+                        max = LenFunc(LBr[i]);
+                        maxind = i;
+                    }
                 }
+                return LBr[maxind];
             }
-            return LBr[maxind];
+            catch
+            {
+                List<List<Struct>> LBr = new List<List<Struct>>();
+                Debug.WriteLine("Ошибка в модуле построения ветвей.");
+                Environment.Exit(1);
+                return LBr[1]; //обход ошибки
+
+
+            }
         }
         /// <summary>
-        /// Метод записи в файл
+        /// Запись в файл
         /// </summary>
         /// <param name="LPathFunc"></param>
         /// <param name="maxind"></param>
         /// <param name="max"></param>
         public void vivod(List<List<Struct>> LPathFunc, int maxind, int max)
         {
-            using (StreamWriter sr = new StreamWriter(@"Вывод.csv", false, Encoding.Default, 10))
+            try
             {
-                foreach (Struct Path in LPathFunc[maxind])
+                using (StreamWriter sr = new StreamWriter(@"Вывод.csv", false, Encoding.Default, 10))
                 {
-                    sr.Write(Path.p1 + " - " + Path.p2 + ";(" + Path.length + ") ");
+                    foreach (Struct Path in LPathFunc[maxind])
+                    {
+                        sr.Write(Path.p1 + " - " + Path.p2 + ";(" + Path.length + ") ");
+                    }
+                    sr.WriteLine("Длина " + max);
                 }
-                sr.WriteLine("Длина " + max);
+            }
+            catch
+            {
+                Debug.WriteLine("Запись в файл прервана.");
+                Environment.Exit(1);
+
             }
         }
 
--- ЭкзаменПМ02/Program.cs
+++ ЭкзаменПМ02/Program.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace ЭкзаменПМ02
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            Debug.Listeners.Add(new TextWriterTraceListener(File.Create("log.txt")));
+            Debug.AutoFlush = true;
+            Class1 c1 = new Class1();
+            c1.resh();
         }
     }
 }
