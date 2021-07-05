53123058a72090d0b34934c5b24d7d61897b58c1
Ky So <klimochkinaff@mail.ru>
2021-07-05 10:18:11 +0300

    добавление структуры

--- ЭкзаменПМ02/Class1.cs
+++ ЭкзаменПМ02/Class1.cs
@@ -8,6 +8,19 @@ namespace ЭкзаменПМ02
 {
     class Class1
     {
+        /// <summary>
+        /// Структура путей и стоимости перемещения
+        /// </summary>
+        public struct Struct
+        {
+            public int p1;
+            public int p2;
+            public int length;
+            public override string ToString()
+            {
+                return p1.ToString() + " - " + p2.ToString() + " " + length.ToString();
+            }
+        }
 
     }
 }
